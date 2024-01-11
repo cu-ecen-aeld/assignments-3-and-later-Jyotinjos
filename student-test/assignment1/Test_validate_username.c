9a192c13928f58635787aa3b6384bde54e465d27
jyotin <jyotin@Ubuntu20.myguest.virtualbox.org>
2024-01-11 06:55:28 +0000

    Refactor

--- student-test/assignment1/Test_validate_username.c
+++ student-test/assignment1/Test_validate_username.c
@@ -14,12 +14,5 @@
 */
 void test_validate_my_username()
 {
-    /**
-     * TODO: Replace the line below with your code here as described above to verify your /conf/username.txt 
-     * config file and my_username() functions are setup properly
-     */
-    const char* Name1 = my_username();
-    char* Name2 = malloc_username_from_conf_file();
-    TEST_ASSERT_EQUAL_STRING_MESSAGE(Name1, Name2, "EQUAL");
-    //TEST_ASSERT_TRUE_MESSAGE(false,"AESD students, please fix me!");
+    TEST_ASSERT_EQUAL_STRING_MESSAGE(my_username(), malloc_username_from_conf_file(), "EQUAL");
 }
